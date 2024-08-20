e484655ff90c1dc3535ab16e1f895d470478dedf
Phoenixgreen763 <148868559+Phoenixgreen763@users.noreply.github.com>
2024-08-20 06:19:08 +0100

    commit

--- products/admin.py
+++ products/admin.py
@@ -1,10 +1,6 @@
 from django.contrib import admin
-from .models import Category, Product  # Correct import path
+from .models import Product, Category
 
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'friendly_name')
-
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'category', 'price')
+# Register your models here.
+admin.site.register(Product)
+admin.site.register(Category)
