7677123658cce77d06d9ae47f2a7c472b9bbf118
Phoenixgreen763 <148868559+Phoenixgreen763@users.noreply.github.com>
2024-09-02 14:21:14 +0100

    finished add product functionality

--- profiles/views.py
+++ profiles/views.py
@@ -6,40 +6,43 @@ from .forms import UserProfileForm
 
 from checkout.models import Order
 
-# Create your views here.
 def profile(request):
+    """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
-    
+
     if request.method == 'POST':
         form = UserProfileForm(request.POST, instance=profile)
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile updated successfully')
-    
-    form = UserProfileForm(instance=profile)
+        else:
+            messages.error(request, 'Update failed. Please ensure the form is valid.')
+    else:
+        form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
-    
+
     template = 'profiles/profile.html'
     context = {
         'form': form,
         'orders': orders,
-        'on_profile_page': True,
+        'on_profile_page': True
     }
-    
+
     return render(request, template, context)
 
-def order_histroy(request, order_number):
+
+def order_history(request, order_number):
     order = get_object_or_404(Order, order_number=order_number)
-    
+
     messages.info(request, (
         f'This is a past confirmation for order number {order_number}. '
-        'A confirmatuion email was sent on the order date.'
+        'A confirmation email was sent on the order date.'
     ))
-    
+
     template = 'checkout/checkout_success.html'
     context = {
         'order': order,
         'from_profile': True,
     }
-    
+
     return render(request, template, context)
