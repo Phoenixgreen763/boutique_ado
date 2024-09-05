6a47d6124cfcc254b724a9812148262522529e3a
Phoenixgreen763 <148868559+Phoenixgreen763@users.noreply.github.com>
2024-09-05 06:04:39 +0100

    c

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['boutique-ado-practice.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['boutique-ado-practice-1fea992b29cd.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
